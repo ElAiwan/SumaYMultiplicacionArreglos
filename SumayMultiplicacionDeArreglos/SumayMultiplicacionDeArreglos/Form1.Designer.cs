da917d0cf66925307e9c88ee1a56752dc5148858
ElAiwan <chavarriaivan97@gmail.com>
2024-09-21 14:16:15 -0600

    Ejercicio 1

--- SumayMultiplicacionDeArreglos/SumayMultiplicacionDeArreglos/Form1.Designer.cs
+++ SumayMultiplicacionDeArreglos/SumayMultiplicacionDeArreglos/Form1.Designer.cs
@@ -28,13 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSimular = new System.Windows.Forms.Button();
+            this.dgvResultados = new System.Windows.Forms.DataGridView();
+            this.lblResultado = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSimular
+            // 
+            this.btnSimular.Location = new System.Drawing.Point(12, 12);
+            this.btnSimular.Name = "btnSimular";
+            this.btnSimular.Size = new System.Drawing.Size(75, 23);
+            this.btnSimular.TabIndex = 0;
+            this.btnSimular.Text = "Simular";
+            this.btnSimular.UseVisualStyleBackColor = true;
+            this.btnSimular.Click += new System.EventHandler(this.btnSimular_Click);
+            // 
+            // dgvResultados
+            // 
+            this.dgvResultados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResultados.Location = new System.Drawing.Point(12, 52);
+            this.dgvResultados.Name = "dgvResultados";
+            this.dgvResultados.ReadOnly = true;
+            this.dgvResultados.Size = new System.Drawing.Size(760, 468);
+            this.dgvResultados.TabIndex = 1;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(12, 523);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(139, 13);
+            this.lblResultado.TabIndex = 2;
+            this.lblResultado.Text = "Presione Simular para iniciar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.dgvResultados);
+            this.Controls.Add(this.btnSimular);
+            this.Name = "Form1";
+            this.Text = "Simulacion de Dados";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSimular;
+        private System.Windows.Forms.DataGridView dgvResultados;
+        private System.Windows.Forms.Label lblResultado;
     }
 }
 
--- SumayMultiplicacionDeArreglos/SumayMultiplicacionDeArreglos/Form1.cs
+++ SumayMultiplicacionDeArreglos/SumayMultiplicacionDeArreglos/Form1.cs
@@ -3,18 +3,77 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SumayMultiplicacionDeArreglos
-{
-    public partial class Form1 : Form
     {
-        public Form1()
+        public partial class Form1 : Form
         {
-            InitializeComponent();
+            public Form1()
+            {
+                InitializeComponent();
+                this.Load += new System.EventHandler(this.Form1_Load);
+            }
+
+            private void btnSimular_Click(object sender, EventArgs e)
+            {
+                SimularDados();
+            }
+
+            private void SimularDados()
+            {
+                int[] frecuencias = new int[11]; // Arreglo para contar sumas de 2 a 12
+
+                Random rand = new Random(); // Generador de números aleatorios
+
+                // Realizar 36000 lanzamientos de dos dados
+                for (int tiro = 0; tiro < 36000; tiro++) // Corrected loop condition
+                {
+                    int dado1 = rand.Next(1, 7); // Lanzar el primer dado
+                    int dado2 = rand.Next(1, 7); // Lanzar el segundo dado
+                    int suma = dado1 + dado2; // Calcular la suma de los valores de los dados
+
+                    frecuencias[suma - 2]++; // Incrementar el contador correspondiente (0-10)
+                }
+
+                // Limpiar el DataGridView antes de llenarlo
+                dgvResultados.Rows.Clear();
+
+                // Llenar el DataGridView con los resultados
+                for (int tiro = 0; tiro < frecuencias.Length; tiro++)
+                {
+                    int suma = tiro + 2;
+                    dgvResultados.Rows.Add(suma, frecuencias[tiro]);
+                }
+
+                // Calcular el porcentaje de veces que aparece el 7
+                double porcentaje = (double)frecuencias[5] / 36000 * 100;
+                lblResultado.Text = $"La suma 7 apareció {frecuencias[5]} veces, lo que representa el {porcentaje}% de los lanzamientos";
+            }
+
+            private void Form1_Load(object sender, EventArgs e)
+            {
+                // Configurar columnas del DataGridView
+                dgvResultados.ColumnCount = 2;
+                dgvResultados.Columns[0].Name = "Suma";
+                dgvResultados.Columns[1].Name = "Frecuencia";
+
+                // Ajustar el tamaño de las columnas para que se vean correctamente
+                dgvResultados.Columns[0].Width = 100;
+                dgvResultados.Columns[1].Width = 150;
+
+                // Evitar que el usuario edite las celdas del DataGridView
+                dgvResultados.ReadOnly = true;
+
+                // Ajustar el modo de tamaño automático de las filas
+                dgvResultados.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+
+                // Centrar el contenido de las celdas
+                dgvResultados.RowsDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            }
         }
     }
-}
